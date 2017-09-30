e425ba7d98ad8edc2dd578912bbfed8e584d87f6
srihari kapu <sriharikapu163@gmail.com>
2017-09-30 16:56:01 +0800

    initial commit

---
+++ Downloads/web-speech-ai-master/index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const APIAI_TOKEN = "6e5ebb5007ae445898d9b1dd8bab40e6";
+const APIAI_SESSION_ID = "7fcbec6cc7a743328afeed92a341dbb5";
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/views')); // html
+app.use(express.static(__dirname + '/public')); // js, css, images
+
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+const io = require('socket.io')(server);
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+const apiai = require('apiai')(APIAI_TOKEN);
+
+// Web UI
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
+io.on('connection', function(socket) {
+  socket.on('chat message', (text) => {
+    console.log('Message: ' + text);
+
+    // Get a reply from API.ai
+
+    let apiaiReq = apiai.textRequest(text, {
+      sessionId: APIAI_SESSION_ID
+    });
+
+    apiaiReq.on('response', (response) => {
+      let aiText = response.result.fulfillment.speech;
+      console.log('Bot reply: ' + aiText);
+      socket.emit('bot reply', aiText);
+    });
+
+    apiaiReq.on('error', (error) => {
+      console.log(error);
+    });
+
+    apiaiReq.end();
+
+  });
+});
